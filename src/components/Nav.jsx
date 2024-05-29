4fe2b93b2966708aa10af007a3181ea99651cd56
Mubeen35951 <166198879+Mubeen35951@users.noreply.github.com>
2024-05-29 10:44:52 +0500

    CHANGING IN NAV

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,7 +7,7 @@ const Nav = () => {
   return (
     <div className='cont1'>
       <ul className="navcontainer">
-        <li className="navcomponents"><Link className="navcomponents" to="">Home</Link></li>
+        <li className="navcomponents"><Link className="navcomponents" to="/">Home</Link></li>
         <li className="navcomponents"><Link className="navcomponents" to="/projectpages">Projects</Link></li>
         <li className="navcomponents"><Link className="navcomponents" to="/contact">Contact</Link></li>
       
